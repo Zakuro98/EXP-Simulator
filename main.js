488367acfac6c65a830901f63e87c252e86c2ed9
Zakuro98 <89255047+Zakuro98@users.noreply.github.com>
2021-08-21 06:01:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = {
+let game = {
     total_exp: 0,
     exp_add: 1,
     level: 1,
@@ -16,7 +16,22 @@ var game = {
 
     amp: 1
 }
-  
+
+function format_num(num) {
+    let input = num.toString()
+    let output
+    if (num >= 1000) {
+        let digits = input.length
+        for (let i=digits-3; i>0; i-=3) {
+            output = input.substr(0,i) + "," + input.substr(i)
+        }
+    } else {
+        output = input
+    }
+
+    return output
+}
+
 function get_level(xp) {
     return Math.floor(Math.pow(27*xp/32,1/3) - 2) + 1
 }
@@ -24,7 +39,7 @@ function get_level(xp) {
 function get_exp(lvl) {
     if (lvl == 0) {
         return lvl
-    }else{
+    } else {
         return 32/27*Math.pow(lvl+2,3)
     }
 }
@@ -50,7 +65,7 @@ function color_update() {
     if (game.level < 60) {
         document.getElementById("lvlnum").style.color = get_color(Math.floor(game.level/10))
         document.getElementById("progress").style.background = get_color(Math.floor(game.level/10))
-    }else{
+    } else {
         document.getElementById("lvlnum").style.color = get_color((Math.floor(game.level/60)+5)%12)
         document.getElementById("progress").style.background = get_color((Math.floor(game.level/60)+5)%12)
     }
@@ -60,19 +75,19 @@ function upgrade_update() {
     if (game.level >= 2) {
         document.getElementById("boost").style.display = "block"
         document.getElementById("boost_button").style.display = "block"
-    }if (game.boost_level < 60) {
+    } if (game.boost_level < 60) {
         if (game.level >= game.boost_level) {
             document.getElementById("boost_button").innerHTML = "UPGRADE!"
             document.getElementById("boost_button").style.color = "#ffffff"
-        }else{
+        } else {
             document.getElementById("boost_button").innerHTML = "LVL " + game.boost_level
             if (game.boost_level < 60) {
                 document.getElementById("boost_button").style.color = get_color(Math.floor(game.boost_level/10))
-            }else{
+            } else {
                 document.getElementById("boost_button").style.color = get_color((Math.floor(game.boost_level/60)+5)%12)
             }
         }
-    }else{
+    } else {
         document.getElementById("boost_button").innerHTML = "MAXED"
         document.getElementById("boost_button").style.color = "#ffffff"
     }
@@ -80,19 +95,19 @@ function upgrade_update() {
     if (game.level >= 5) {
         document.getElementById("auto").style.display = "block"
         document.getElementById("auto_button").style.display = "block"
-    }if (game.auto_level < 60) {
+    } if (game.auto_level < 60) {
         if (game.level >= game.auto_level){
             document.getElementById("auto_button").innerHTML = "UPGRADE!"
             document.getElementById("auto_button").style.color = "#ffffff"
-        }else{
+        } else {
             document.getElementById("auto_button").innerHTML = "LVL " + game.auto_level
             if (game.auto_level < 60) {
                 document.getElementById("auto_button").style.color = get_color(Math.floor(game.auto_level/10))
-            }else{
+            } else {
                 document.getElementById("auto_button").style.color = get_color((Math.floor(game.auto_level/60)+5)%12)
             }
         }
-    }else{
+    } else {
         document.getElementById("auto_button").innerHTML = "MAXED"
         document.getElementById("auto_button").style.color = "#ffffff"
     }
@@ -100,10 +115,10 @@ function upgrade_update() {
     if (game.level >= 60 || game.amp > 1) {
         document.getElementById("amplifier").style.display = "block"
         document.getElementById("amp_button").style.display = "block"
-    }if (game.level >= 60) {
+    } if (game.level >= 60) {
         document.getElementById("amp_button").innerHTML = "PRESTIGE!"
         document.getElementById("amp_button").style.color = "#ffffff"
-    }else{
+    } else {
         document.getElementById("amp_button").innerHTML = "LVL 60"
         document.getElementById("amp_button").style.color = get_color(6)
     }
@@ -114,7 +129,7 @@ function increment(num) {
         game.total_exp += num
         if (game.total_exp <= 10) {
             game.level = 1
-        }else{
+        } else {
             game.level = get_level(game.total_exp)
         }
 
@@ -122,19 +137,17 @@ function increment(num) {
         game.goal = Math.ceil(get_exp(game.level) - get_exp(game.level-1))
 
         document.getElementById("progress").style.width = 100*game.exp/game.goal + "%"
-    }else{
+    } else {
         document.getElementById("progress").style.width = 100 + "%"
     }
 
     color_update()
     upgrade_update()
 
-    document.getElementById("lvlnum").innerHTML = game.level
-    if (game.level < 60)
-        document.getElementById("exp").innerHTML = game.exp + " / " + game.goal + " EXP"
-    else
-        document.getElementById("exp").innerHTML = "Maxed!"
-    document.getElementById("total_exp").innerHTML = game.total_exp + " Total EXP"
+    document.getElementById("lvlnum").innerHTML = format_num(game.level)
+    if (game.level < 60) document.getElementById("exp").innerHTML = format_num(game.exp) + " / " + format_num(game.goal) + " EXP"
+    else document.getElementById("exp").innerHTML = "Maxed!"
+    document.getElementById("total_exp").innerHTML = format_num(game.total_exp) + " Total EXP"
 }
 
 function tick() {
@@ -155,13 +168,13 @@ function upgrade(id) {
                     game.boost_tier += 1
                     game.boost_level += 2
                     game.exp_add = (game.boost_tier + 1)*game.amp
-                    document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + game.boost_tier + ": +" + game.exp_add + " EXP/click"
-                    document.getElementById("click").innerHTML = "+" + game.exp_add + " EXP"
-                }if (game.level < game.boost_level) {
-                    document.getElementById("boost_button").innerHTML = "LVL " + game.boost_level
+                    document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + format_num(game.boost_tier) + ": +" + format_num(game.exp_add) + " EXP/click"
+                    document.getElementById("click").innerHTML = "+" + format_num(game.exp_add) + " EXP"
+                } if (game.level < game.boost_level) {
+                    document.getElementById("boost_button").innerHTML = "LVL " + format_num(game.boost_level)
                     if (game.auto_level < 60) {
                         document.getElementById("boost_button").style.color = get_color(Math.floor(game.boost_level/10))
-                    }else{
+                    } else {
                         document.getElementById("boost_button").style.color = get_color((Math.floor(game.boost_level/60)+5)%12)
                     }
                 }
@@ -174,12 +187,12 @@ function upgrade(id) {
                     game.auto_tier += 1
                     game.auto_level += 5
                     game.cps = 2*game.auto_tier
-                    document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + game.auto_tier + ": " + game.cps + " clicks/s"
-                }if (game.level < game.auto_level) {
-                    document.getElementById("auto_button").innerHTML = "LVL " + game.auto_level
+                    document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + format_num(game.auto_tier) + ": " + format_num(game.cps) + " clicks/s"
+                } if (game.level < game.auto_level) {
+                    document.getElementById("auto_button").innerHTML = "LVL " + format_num(game.auto_level)
                     if (game.auto_level < 60) {
                         document.getElementById("auto_button").style.color = get_color(Math.floor(game.auto_level/10))
-                    }else{
+                    } else {
                         document.getElementById("auto_button").style.color = get_color((Math.floor(game.auto_level/60)+5)%12)
                     }
                 }
@@ -191,7 +204,7 @@ function upgrade(id) {
 
 function prestige() {
     game.amp += 1
-    document.getElementById("amplifier").innerHTML = "+1 AMP<br>Current: " + game.amp + " AMP"
+    document.getElementById("amplifier").innerHTML = "+1 AMP<br>Current: " + format_num(game.amp) + " AMP"
     document.getElementById("amp_button").innerHTML = "LVL 60"
     document.getElementById("amp_button").style.color = get_color(6)
 
@@ -217,23 +230,23 @@ function prestige() {
     color_update()
 
     document.getElementById("lvlnum").innerHTML = game.level
-    document.getElementById("exp").innerHTML = game.exp + " / " + game.goal + " EXP"
-    document.getElementById("total_exp").innerHTML = game.total_exp + " Total EXP"
+    document.getElementById("exp").innerHTML = format_num(game.exp) + " / " + format_num(game.goal) + " EXP"
+    document.getElementById("total_exp").innerHTML = format_num(game.total_exp) + " Total EXP"
 
-    document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + game.boost_tier + ": +" + game.exp_add + " EXP/click"
+    document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + format_num(game.boost_tier) + ": +" + format_num(game.exp_add) + " EXP/click"
     document.getElementById("boost_button").innerHTML = "UPGRADE!"
-    document.getElementById("click").innerHTML = "+" + game.exp_add + " EXP"
-    document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + game.auto_tier + ": " + game.cps + " clicks/s"
+    document.getElementById("click").innerHTML = "+" + format_num(game.exp_add) + " EXP"
+    document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + format_num(game.auto_tier) + ": " + format_num(game.cps) + " clicks/s"
     document.getElementById("auto_button").innerHTML = "UPGRADE!"
 
     document.getElementById("progress").style.width = 0 + "%"
 }
 
-var tick_loop = window.setInterval(function() {
+let tick_loop = window.setInterval(function() {
     tick()
 }, 1000/game.tickspeed)
 
-var savegame = JSON.parse(localStorage.getItem("exp_simulator_save"))
+let savegame = JSON.parse(localStorage.getItem("exp_simulator_save"))
 if (savegame !== null) {
     game = savegame
 }
@@ -241,19 +254,19 @@ if (savegame !== null) {
 color_update()
 upgrade_update()
 
-document.getElementById("amplifier").innerHTML = "+1 AMP<br>Current: " + game.amp + " AMP"
-document.getElementById("lvlnum").innerHTML = game.level
-document.getElementById("exp").innerHTML = game.exp + " / " + game.goal + " EXP"
-document.getElementById("total_exp").innerHTML = game.total_exp + " Total EXP"
-document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + game.boost_tier + ": +" + game.exp_add + " EXP/click"
-document.getElementById("click").innerHTML = "+" + game.exp_add + " EXP"
-document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + game.auto_tier + ": " + game.cps + " clicks/s"
+document.getElementById("amplifier").innerHTML = "+1 AMP<br>Current: " + format_num(game.amp) + " AMP"
+document.getElementById("lvlnum").innerHTML = format_num(game.level)
+document.getElementById("exp").innerHTML = format_num(game.exp) + " / " + format_num(game.goal) + " EXP"
+document.getElementById("total_exp").innerHTML = format_num(game.total_exp) + " Total EXP"
+document.getElementById("boost").innerHTML = "EXP Boost <br> Tier " + format_num(game.boost_tier) + ": +" + format_num(game.exp_add) + " EXP/click"
+document.getElementById("click").innerHTML = "+" + format_num(game.exp_add) + " EXP"
+document.getElementById("auto").innerHTML = "Autoclicker <br> Tier " + format_num(game.auto_tier) + ": " + format_num(game.cps) + " clicks/s"
 if (game.level < 60) {
     document.getElementById("progress").style.width = 100*game.exp/game.goal + "%"
-}else{
+} else {
     document.getElementById("progress").style.width = 100 + "%"
 }
 
-var save_loop = window.setInterval(function() {
+let save_loop = window.setInterval(function() {
     localStorage.setItem("exp_simulator_save",JSON.stringify(game))
 }, 15000)
