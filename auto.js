0e48c3bec2cf1e5880b1864d3ed8d5d4897dbad1
Zakuro <89255047+Zakuro98@users.noreply.github.com>
2021-12-24 17:51:01 -0900

    several new automation toggles

--- auto.js
+++ auto.js
@@ -99,14 +99,29 @@ function oc_toggle() {
 
 //discharge automation toggle
 function ds_toggle() {
-    if (!game.autods_toggle) {
-        game.autods_toggle = true
-        document.getElementById("dis_auto").innerText = "ON"
-        document.getElementById("dis_auto").style.color = "#00ff00"
+    game.autods_toggle++
+    if (game.perks[11]) {
+        if (game.autods_toggle > 2) game.autods_toggle = 0
     } else {
-        game.autods_toggle = false
-        document.getElementById("dis_auto").innerText = "OFF"
-        document.getElementById("dis_auto").style.color = "#ff0000"
+        if (game.autods_toggle > 1) game.autods_toggle = 0
+    }
+    switch (game.autods_toggle) {
+        case 0:
+            document.getElementById("dis_auto").innerText = "OFF"
+            document.getElementById("dis_auto").style.color = "#ff0000"
+            document.getElementById("dis_text").style.display = "block"
+            document.getElementById("dis_input").style.display = "block"
+            break
+        case 1:
+            document.getElementById("dis_auto").innerText = "ON"
+            document.getElementById("dis_auto").style.color = "#00ff00"
+            break
+        case 2:
+            document.getElementById("dis_auto").innerText = "SMART"
+            document.getElementById("dis_auto").style.color = "#00ffff"
+            document.getElementById("dis_text").style.display = "none"
+            document.getElementById("dis_input").style.display = "none"
+            break
     }
 }
 
@@ -123,137 +138,96 @@ function pp_toggle() {
     }
 }
 
+//capacitance mode switch automation toggle
+function cp_toggle() {
+    if (!game.autocp_toggle) {
+        game.autocp_toggle = true
+        document.getElementById("cap_auto").innerText = "ON"
+        document.getElementById("cap_auto").style.color = "#00ff00"
+        if (game.pp_bought[38]) set_capacitance(4)
+        else if (game.pp_bought[37]) set_capacitance(3)
+        else if (game.pp_bought[35]) set_capacitance(2)
+        else if (game.pp_bought[32]) set_capacitance(1)
+    } else {
+        game.autocp_toggle = false
+        document.getElementById("cap_auto").innerText = "OFF"
+        document.getElementById("cap_auto").style.color = "#ff0000"
+    }
+}
+
+//reboot automation toggle
+function rb_toggle() {
+    if (!game.confirmation) {
+        if (!game.autorb_toggle) {
+            game.autorb_toggle = true
+            document.getElementById("autorb_toggle").innerText = "ENABLED"
+            document.getElementById("autorb_toggle").style.color = "#00ff00"
+        } else {
+            game.autorb_toggle = false
+            document.getElementById("autorb_toggle").innerText = "DISABLED"
+            document.getElementById("autorb_toggle").style.color = "#ff0000"
+        }
+    } else {
+        alert(
+            "Reboot Confirmation must be turned off to enable Reboot automation"
+        )
+    }
+}
+
+//pending reboot toggle
+function pendingrb_toggle() {
+    if (!game.autorb_pending) {
+        game.autorb_pending = true
+        document.getElementById("pendingrb_toggle").innerText = "YES"
+    } else {
+        game.autorb_pending = false
+        document.getElementById("pendingrb_toggle").innerText = "NO"
+    }
+}
+
+//smart autoprestige toggle
+function smart_toggle() {
+    if (!game.smartpr_toggle) {
+        game.smartpr_toggle = true
+        if (game.smartpr_mode === 0) autopr_switch(4)
+        else if (game.smartpr_mode === 1) autopr_switch(2)
+        document.getElementById("smart_toggle").innerText = "ENABLED"
+        document.getElementById("smart_toggle").style.color = "#00ff00"
+    } else {
+        game.smartpr_toggle = false
+        document.getElementById("smart_toggle").innerText = "DISABLED"
+        document.getElementById("smart_toggle").style.color = "#ff0000"
+    }
+}
+
+//initial smart autoprestige mode
+function initial_switch(mode) {
+    game.smartpr_start = mode
+
+    document.getElementById("smart_peak_mode").className = "button"
+    document.getElementById("smart_pp_mode").className = "button"
+
+    switch (mode) {
+        case 0:
+            document.getElementById("smart_peak_mode").className =
+                "button mode_active"
+            break
+        case 1:
+            document.getElementById("smart_pp_mode").className =
+                "button mode_active"
+            break
+    }
+}
+
 //battery mode toggle
 function battery_toggle() {
     if (game.battery_mode === 0) {
         game.battery_mode = 1
-        click_update()
-        document.getElementById("boost").innerText =
-            "EXP Boost\nTier " +
-            format_num(
-                game.boost_tier + game.starter_kit + game.generator_kit
-            ) +
-            ": +" +
-            format_num(
-                Math.round(
-                    game.exp_add *
-                        game.global_multiplier *
-                        game.exp_battery *
-                        game.cap_boost
-                )
-            ) +
-            " EXP/click"
-        if (game.autods_toggle && game.autods_goal === 0)
-            document.getElementById("boost").innerText =
-                "EXP Boost\nTier " +
-                format_num(
-                    game.boost_tier + game.starter_kit + game.generator_kit
-                ) +
-                ": +" +
-                format_num(
-                    Math.round(
-                        game.exp_add *
-                            game.global_multiplier *
-                            game.exp_battery *
-                            (game.cap_boost +
-                                (1 - game.cap_boost) * game.cap_mode * 2)
-                    )
-                ) +
-                " EXP/click"
-        document.getElementById("fluct").innerText =
-            "EXP Fluctuation\nTier " +
-            format_num(
-                game.fluct_tier + game.starter_kit + game.generator_kit
-            ) +
-            ": +" +
-            format_num(
-                Math.round(
-                    game.exp_fluct *
-                        game.global_multiplier *
-                        game.exp_battery *
-                        game.cap_boost
-                )
-            ) +
-            " max extra EXP/click"
-        if (game.autods_toggle && game.autods_goal === 0)
-            document.getElementById("fluct").innerText =
-                "EXP Fluctuation\nTier " +
-                format_num(
-                    game.fluct_tier + game.starter_kit + game.generator_kit
-                ) +
-                ": +" +
-                format_num(
-                    Math.round(
-                        game.exp_fluct *
-                            game.global_multiplier *
-                            game.exp_battery *
-                            (game.cap_boost +
-                                (1 - game.cap_boost) * game.cap_mode * 2)
-                    )
-                ) +
-                " max extra EXP/click"
         document.getElementById("battery_mode").innerText = "IDLE"
         document.getElementById("battery_mode").style.color = "#00ffff"
     } else {
         game.battery_mode = 0
-        click_update()
         document.getElementById("battery_mode").innerText = "ACTIVE"
-        document.getElementById("boost").innerText =
-            "EXP Boost\nTier " +
-            format_num(
-                game.boost_tier + game.starter_kit + game.generator_kit
-            ) +
-            ": +" +
-            format_num(
-                Math.round(
-                    game.exp_add * game.global_multiplier * game.cap_boost
-                )
-            ) +
-            " EXP/click"
-        if (game.autods_toggle && game.autods_goal === 0)
-            document.getElementById("boost").innerText =
-                "EXP Boost\nTier " +
-                format_num(
-                    game.boost_tier + game.starter_kit + game.generator_kit
-                ) +
-                ": +" +
-                format_num(
-                    Math.round(
-                        game.exp_add *
-                            game.global_multiplier *
-                            (game.cap_boost +
-                                (1 - game.cap_boost) * game.cap_mode * 2)
-                    )
-                ) +
-                " EXP/click"
-        document.getElementById("fluct").innerText =
-            "EXP Fluctuation\nTier " +
-            format_num(
-                game.fluct_tier + game.starter_kit + game.generator_kit
-            ) +
-            ": +" +
-            format_num(
-                Math.round(
-                    game.exp_fluct * game.global_multiplier * game.cap_boost
-                )
-            ) +
-            " max extra EXP/click"
-        if (game.autods_toggle && game.autods_goal === 0)
-            document.getElementById("fluct").innerText =
-                "EXP Fluctuation\nTier " +
-                format_num(
-                    game.fluct_tier + game.starter_kit + game.generator_kit
-                ) +
-                ": +" +
-                format_num(
-                    Math.round(
-                        game.exp_fluct *
-                            game.global_multiplier *
-                            (game.cap_boost +
-                                (1 - game.cap_boost) * game.cap_mode * 2)
-                    )
-                ) +
-                " max extra EXP/click"
         document.getElementById("battery_mode").style.color = "#ff0000"
     }
 }
@@ -332,8 +306,11 @@ function toggle_all_automation() {
     if (
         game.autopr_toggle ||
         game.autooc_toggle ||
-        game.autods_toggle ||
-        game.autopp_toggle
+        game.autods_toggle >= 1 ||
+        game.autopp_toggle ||
+        game.autocp_toggle ||
+        game.autorb_toggle ||
+        game.smartpr_toggle
     )
         all_off = false
 
@@ -343,16 +320,22 @@ function toggle_all_automation() {
         }
         game.autopr_toggle = true
         game.autooc_toggle = true
-        game.autods_toggle = true
+        game.autods_toggle = 1
         game.autopp_toggle = true
+        game.autocp_toggle = true
+        game.autorb_toggle = true
+        game.smartpr_toggle = true
     } else {
         for (let i = 0; i < 6; i++) {
             game.autoup_toggle[i] = false
         }
         game.autopr_toggle = false
         game.autooc_toggle = false
-        game.autods_toggle = false
+        game.autods_toggle = 0
         game.autopp_toggle = false
+        game.autocp_toggle = false
+        game.autorb_toggle = false
+        game.smartpr_toggle = false
     }
 
     for (let i = 0; i < 6; i++) {
@@ -361,12 +344,19 @@ function toggle_all_automation() {
     }
     pr_toggle()
     pr_toggle()
+    smart_toggle()
+    smart_toggle()
     oc_toggle()
     oc_toggle()
     ds_toggle()
     ds_toggle()
+    if (game.perks[11]) ds_toggle()
     pp_toggle()
     pp_toggle()
+    cp_toggle()
+    cp_toggle()
+    rb_toggle()
+    rb_toggle()
     battery_toggle()
     battery_toggle()
 }
